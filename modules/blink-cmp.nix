8780affcb1e59f4813bdfe9e8e64864855d3efb9
Christian Taylor <christianjtaylor@sky.com>
2025-08-25 08:04:30 +0100

    fix blink-cmp config

--- modules/blink-cmp.nix
+++ modules/blink-cmp.nix
@@ -5,6 +5,70 @@
     enable = true;
 
     settings = {
+      completion = {
+        accept = {
+          create_undo_point = true;
+          auto_brackets = {
+            enabled = true;
+          };
+        };
+        documentation = {
+          auto_show = true;
+          treesitter_highlighting = true;
+        };
+        ghost_text.enabled = true;
+        menu = {
+          enabled = true;
+          auto_show = true;
+        };
+      };
+
+      keymap = {
+        "<C-f>" = [
+          "scroll_documentation_up"
+          "fallback"
+        ];
+        "<C-e>" = [
+          "hide"
+        ];
+        "<C-b>" = [
+          "scroll_documentation_down"
+          "fallback"
+        ];
+        "<C-n>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<C-p>" = [
+          "select_prev"
+          "fallback"
+        ];
+        "<C-space>" = [
+          "show"
+          "show_documentation"
+          "hide_documentation"
+        ];
+        "<C-y>" = [
+          "select_and_accept"
+        ];
+        "<Down>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<S-Tab>" = [
+          "snippet_backward"
+          "fallback"
+        ];
+        "<Tab>" = [
+          "snippet_forward"
+          "fallback"
+        ];
+        "<Up>" = [
+          "select_prev"
+          "fallback"
+        ];
+      };
+
       sources = {
         default = [
           "lsp"
@@ -13,25 +77,6 @@
           "buffer"
           "copilot"
         ];
-
-        completion = {
-          accept = {
-            create_undo_point = true;
-            auto_brackets = {
-              enabled = true;
-            };
-          };
-          documentation = {
-            auto_show = true;
-            treesitter_highlighting = true;
-          };
-          ghost_text.enabled = true;
-          menu = {
-            enabled = true;
-            auto_show = true;
-          };
-        };
-
         providers = {
           copilot = {
             async = true;
@@ -49,52 +94,6 @@
               };
             };
           };
-        };
-
-        keymap = {
-          "<C-f>" = [
-            "scroll_documentation_up"
-            "fallback"
-          ];
-          "<C-e>" = [
-            "hide"
-          ];
-          "<C-b>" = [
-            "scroll_documentation_down"
-            "fallback"
-          ];
-          "<C-n>" = [
-            "select_next"
-            "fallback"
-          ];
-          "<C-p>" = [
-            "select_prev"
-            "fallback"
-          ];
-          "<C-space>" = [
-            "show"
-            "show_documentation"
-            "hide_documentation"
-          ];
-          "<C-y>" = [
-            "select_and_accept"
-          ];
-          "<Down>" = [
-            "select_next"
-            "fallback"
-          ];
-          "<S-Tab>" = [
-            "snippet_backward"
-            "fallback"
-          ];
-          "<Tab>" = [
-            "snippet_forward"
-            "fallback"
-          ];
-          "<Up>" = [
-            "select_prev"
-            "fallback"
-          ];
         };
       };
     };
