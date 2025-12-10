d9fd3d5ec5fa318d8b4bbc485757792f0cd7bbe8
Christian Taylor <christianjtaylor@sky.com>
2025-12-10 07:54:59 +0000

    feat: add comment

--- flake.nix
+++ flake.nix
@@ -7,65 +7,66 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    inputs @ { self
-    , nixpkgs
-    , nixvim
-    , flake-utils
-    , ...
-    }:
-    let
-      inherit (flake-utils.lib) eachDefaultSystem mkApp;
-    in
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-utils,
+    ...
+  }: let
+    inherit (flake-utils.lib) eachDefaultSystem mkApp;
+  in
     eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      system: let
+        pkgs = import nixpkgs {inherit system;};
         makeNixvim = nixvim.legacyPackages.${system}.makeNixvim;
 
         myNixvim = makeNixvim {
-          extraPackages = [
-            pkgs.clang
-            pkgs.pyright # Python language server
-            pkgs.python3 # Python interpreter
+          extraPackages =
+            [
+              pkgs.clang
+              pkgs.pyright # Python language server
+              pkgs.python3 # Python interpreter
 
-            # .NET/C# development
-            pkgs.dotnet-sdk_8 # .NET 8 SDK for C# development
-            pkgs.omnisharp-roslyn # C# LSP server
+              # .NET/C# development
+              pkgs.dotnet-sdk_8 # .NET 8 SDK for C# development
+              pkgs.omnisharp-roslyn # C# LSP server
 
-            # Go development  
-            pkgs.go # Go compiler and tools
-            pkgs.gopls # Go LSP server
+              # Go development
+              pkgs.go # Go compiler and tools
+              pkgs.gopls # Go LSP server
 
-            # Zig development
-            pkgs.zig # Zig compiler
-            pkgs.zls # Zig LSP server
+              # Zig development
+              pkgs.zig # Zig compiler
+              pkgs.zls # Zig LSP server
 
-            # Ruby development
-            pkgs.ruby # Ruby interpreter
-            pkgs.rubyPackages.solargraph # Ruby LSP server
+              # Ruby development
+              pkgs.ruby # Ruby interpreter
+              pkgs.rubyPackages.solargraph # Ruby LSP server
 
-            # JavaScript/TypeScript development
-            pkgs.nodejs_20 # Node.js runtime
-            pkgs.nodePackages.typescript # TypeScript compiler
-            pkgs.nodePackages.typescript-language-server # TS LSP server
+              # JavaScript/TypeScript development
+              pkgs.nodejs_20 # Node.js runtime
+              pkgs.nodePackages.typescript # TypeScript compiler
+              pkgs.nodePackages.typescript-language-server # TS LSP server
 
-            # Java development
-            pkgs.jdk21 # Java Development Kit
-            pkgs.jdt-language-server # Java LSP server
+              # Java development
+              pkgs.jdk21 # Java Development Kit
+              pkgs.jdt-language-server # Java LSP server
 
-            # Kotlin development
-            pkgs.kotlin # Kotlin compiler
-            pkgs.kotlin-language-server # Kotlin LSP server
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            pkgs.xclip # X11 clipboard
-          ];
+              # Kotlin development
+              pkgs.kotlin # Kotlin compiler
+              pkgs.kotlin-language-server # Kotlin LSP server
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.xclip # X11 clipboard
+            ];
 
           imports = [
             ./modules/_top-level.nix
             ./modules/keymaps.nix
             ./modules/autocmds.nix
             ./modules/autosave.nix
+            ./modules/comment.nix
             ./modules/telescope.nix
             ./modules/treesitter.nix
             ./modules/frecency.nix
@@ -95,42 +96,44 @@
         };
 
         myNixvimTerminal = makeNixvim {
-          extraPackages = [
-            pkgs.clang
-            pkgs.pyright # Python language server
-            pkgs.python3 # Python interpreter
+          extraPackages =
+            [
+              pkgs.clang
+              pkgs.pyright # Python language server
+              pkgs.python3 # Python interpreter
 
-            # .NET/C# development
-            pkgs.dotnet-sdk_8 # .NET 8 SDK for C# development
-            pkgs.omnisharp-roslyn # C# LSP server
+              # .NET/C# development
+              pkgs.dotnet-sdk_8 # .NET 8 SDK for C# development
+              pkgs.omnisharp-roslyn # C# LSP server
 
-            # Go development  
-            pkgs.go # Go compiler and tools
-            pkgs.gopls # Go LSP server
+              # Go development
+              pkgs.go # Go compiler and tools
+              pkgs.gopls # Go LSP server
 
-            # Zig development
-            pkgs.zig # Zig compiler
-            pkgs.zls # Zig LSP server
+              # Zig development
+              pkgs.zig # Zig compiler
+              pkgs.zls # Zig LSP server
 
-            # Ruby development
-            pkgs.ruby # Ruby interpreter
-            pkgs.rubyPackages.solargraph # Ruby LSP server
+              # Ruby development
+              pkgs.ruby # Ruby interpreter
+              pkgs.rubyPackages.solargraph # Ruby LSP server
 
-            # JavaScript/TypeScript development
-            pkgs.nodejs_20 # Node.js runtime
-            pkgs.nodePackages.typescript # TypeScript compiler
-            pkgs.nodePackages.typescript-language-server # TS LSP server
+              # JavaScript/TypeScript development
+              pkgs.nodejs_20 # Node.js runtime
+              pkgs.nodePackages.typescript # TypeScript compiler
+              pkgs.nodePackages.typescript-language-server # TS LSP server
 
-            # Java development
-            pkgs.jdk21 # Java Development Kit
-            pkgs.jdt-language-server # Java LSP server
+              # Java development
+              pkgs.jdk21 # Java Development Kit
+              pkgs.jdt-language-server # Java LSP server
 
-            # Kotlin development
-            pkgs.kotlin # Kotlin compiler
-            pkgs.kotlin-language-server # Kotlin LSP server
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            pkgs.xclip # X11 clipboard
-          ];
+              # Kotlin development
+              pkgs.kotlin # Kotlin compiler
+              pkgs.kotlin-language-server # Kotlin LSP server
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.xclip # X11 clipboard
+            ];
 
           imports = [
             ./modules/_top-level.nix
@@ -175,14 +178,13 @@
           tree-sitter
           just
         ];
-      in
-      {
+      in {
         packages.default = myNixvim.overrideAttrs (oldAttrs: {
           meta = {
             description = "Nixvim configuration for Neovim (GUI-optimized)";
             homepage = "https://github.com/ChrisJTaylor/nixvim-config";
             license = pkgs.lib.licenses.mit;
-            maintainers = [ "ChrisJTaylor" ];
+            maintainers = ["ChrisJTaylor"];
           };
         });
 
@@ -191,7 +193,7 @@
             description = "Nixvim configuration for Neovim (Terminal-optimized)";
             homepage = "https://github.com/ChrisJTaylor/nixvim-config";
             license = pkgs.lib.licenses.mit;
-            maintainers = [ "ChrisJTaylor" ];
+            maintainers = ["ChrisJTaylor"];
           };
         });
 
@@ -216,13 +218,13 @@
         formatter = pkgs.nixpkgs-fmt;
 
         checks = {
-          nixfmt = pkgs.runCommand "check-nixfmt" { buildInputs = [ pkgs.nixpkgs-fmt ]; } ''
+          nixfmt = pkgs.runCommand "check-nixfmt" {buildInputs = [pkgs.nixpkgs-fmt];} ''
             echo "Checking Nix formatting..."
             nixpkgs-fmt --check ${./modules} ${./flake.nix} || exit 1
             touch $out
           '';
 
-          stylua = pkgs.runCommand "check-stylua" { buildInputs = [ pkgs.stylua ]; } ''
+          stylua = pkgs.runCommand "check-stylua" {buildInputs = [pkgs.stylua];} ''
             echo "Checking Lua formatting..."
             stylua --check ${./modules} || exit 1
             touch $out
---
+++ modules/comment.nix
@@ -0,0 +1,15 @@
+{...}: {
+  plugins = {
+    comment = {
+      enable = true;
+      settings = {
+        basic = true;
+        extra = true;
+        toggler = {
+          block = "gbc";
+          line = "gcc";
+        };
+      };
+    };
+  };
+}
